6ca9d05b08780391608d8702868d924972f75d18
Wuxin Yang <yangwuxin413@gmail.com>
2023-06-13 12:09:12 -0400

    container change can trigger workflow?

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import MarketingApp from './components/MarketingApp';
 export default () => {
     return <React.Fragment>
-        <h1>Hi Container!</h1>
+        <h1>Hi Container! Run the workflow</h1>
     <MarketingApp/>
     </React.Fragment>;
 };

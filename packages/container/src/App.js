80f3a76d5939e0434f010a459fdea1f117efc74c
Wuxin Yang <yangwuxin413@gmail.com>
2023-06-14 11:02:36 -0400

    container dve modification1

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import MarketingApp from './components/MarketingApp';
 export default () => {
     return <React.Fragment>
-        <h1>Hi Container! AWS set up</h1>
+        <h1>Hi Container! Hello again</h1>
     <MarketingApp/>
     </React.Fragment>;
 };

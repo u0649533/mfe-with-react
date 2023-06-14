71685f911c74e32b0c4f1b8fe018400f363f1fc1
Wuxin Yang <yangwuxin413@gmail.com>
2023-06-14 15:35:40 -0400

    testing

--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -13,14 +13,15 @@ const generateClassName = createGenerateClassName({
 export default () => {
   return (
     <div>
-      <StylesProvider generateClassName={generateClassName}>
+      Hello!!!
+      {/* <StylesProvider generateClassName={generateClassName}>
         <BrowserRouter>
           <Switch>
             <Route exact path="/pricing" component={Pricing}></Route>
             <Route exact path="/" component={Landing}></Route>
           </Switch>
         </BrowserRouter>
-      </StylesProvider>
+      </StylesProvider> */}
     </div>
   );
 };

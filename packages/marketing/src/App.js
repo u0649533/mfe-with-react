79eea68fa03f6492e8933e1bcd51f409079be9ee
Wuxin Yang <yangwuxin413@gmail.com>
2023-06-14 16:07:45 -0400

    added

--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -1,27 +1,21 @@
 import React from "react";
 import { Switch, Route, BrowserRouter } from "react-router-dom";
-import { StylesProvider, createGenerateClassName } from "@material-ui/core/styles";
+import { StylesProvider } from "@material-ui/core/styles";
 
 import Landing from "./components/Landing";
 import Pricing from "./components/Pricing";
 
-
-const generateClassName = createGenerateClassName({
-  productionPrefix: 'ma'
-})
-
 export default () => {
   return (
     <div>
-      Hello marketing
-    {/* <StylesProvider generateClassName={generateClassName}>
-      <BrowserRouter>
-        <div>
-          <Route exact path="/pricing" component={Pricing} />
-          <Route path="/" component={Landing} />
-        </div>
-      </BrowserRouter>
-    </StylesProvider> */}
-  </div>
+      <StylesProvider>
+        <BrowserRouter>
+          <div>
+            <Route exact path="/pricing" component={Pricing} />
+            <Route path="/" component={Landing} />
+          </div>
+        </BrowserRouter>
+      </StylesProvider>
+    </div>
   );
 };
